8f174b0c9a96daeb1df566dfacef9e0f84b2df59
Andre Chaves <andrechavesg@gmail.com>
2018-02-23 13:27:10 -0300

    capitulo5

--- mercado/application/controllers/produtos.php
+++ mercado/application/controllers/produtos.php
@@ -11,6 +11,7 @@ class Produtos extends CI_Controller{
 		
 		$this->load->helper("url");
 		$this->load->helper("currency");
+		$this->load->helper(array("form"));
 		$this->load->view("produtos/index.php", $dados);
     }
 }
